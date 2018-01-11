060a6f77cc4faea7dbc546eb66aa493a19c145ad
Simon Duret <simon.duret@etu.u-bordeaux.fr>
2018-01-11 16:30:37 +0100

    Implementing options parsing using getopt (from command line)

---
+++ sslmitm.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8
+
+"""
+This script implements the SSLstrip attack presented by Moxie at the BlackHat
+conference, in 2008.
+"""
+
+__author__  = 'Amélie Risi, Brendan Guevel and Simon Duret'
+__version__ = '0.1'
+__license__ = 'GPL'
+__status__  = 'Development'
+
+
+from sslmitm.Options import Options
+
+if __name__ == "__main__":
+    options = Options(__author__, __version__, __license__)
+    options.parse()
