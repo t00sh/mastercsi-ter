142cf29e558f3e145522b690df23adf55df6e53c
amelie <amelie@amelie-pc.home>
2018-02-05 23:03:54 +0100

    Nginx: try to config nginx but not tested

--- sslstrip/opeth/start.sh
+++ sslstrip/opeth/start.sh
@@ -2,3 +2,8 @@
 hostname opeth
 ifconfig eth0 147.210.12.1/24
 route add default gw 147.210.12.2
+
+### NGINX load new configuration ###
+nginx -c /mnt/host/nginx.conf
+## Restart service ##
+service nginx reload
