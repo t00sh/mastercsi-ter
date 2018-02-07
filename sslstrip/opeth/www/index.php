b0157458c8e15fd805605481a50f1e0d0ecfb91b
Simon Duret <simon.duret@etu.u-bordeaux.fr>
2018-02-07 13:51:02 +0100

    sslstrip (www): POST on secure.php instead of index.php, and print username on secure.php

--- sslstrip/opeth/www/index.php
+++ sslstrip/opeth/www/index.php
@@ -1,21 +1,3 @@
-<?php
-    session_start();
-    if(isset($_POST["submit"])){
-        $users = array(
-                        "user" => "toto",
-                        "admin"=>"password"          
-                );
-        $username = isset($_POST["username"]) ? $_POST["username"] : "";
-        $password = isset($_POST["password"]) ? $_POST["password"] : "";
-        if(isset($users[$username]) && $password == $users[$username]){
-            $_SESSION["user_data"]["username"] = $users[$username];
-            header("location: https://147.210.12.1/secure.php");
-            exit();
-        }else{
-            $msg = "Invalid Login";
-        }
-    }
-?>
 <html>
   <head>
     <title> Index </title>
@@ -23,11 +5,11 @@
   <body>
     <h1>Welcome to our page</h1>
 
-    <form action="" method="post">
-        <p><?php if (isset($msg)) echo $msg;?></p>
-        <p>Username : <input type="text" name="username" id="username" autocomplete="off"/></p>
-        <p>Password : <input type="password" name="password" id="password" autocomplete="off"/></p>
-        <input type="submit" name="submit" value="Login" autocomplete="off">
+    <form action="https://147.210.12.1/secure.php" method="post">
+      <p>Login :</p>
+      <p>Username : <input type="text" name="username" id="username" autocomplete="off"/></p>
+      <p>Password : <input type="password" name="password" id="password" autocomplete="off"/></p>
+      <input type="submit" name="submit" value="Login" autocomplete="off">
     </form>
   </body>
 </html>
--- sslstrip/opeth/www/secure.php
+++ sslstrip/opeth/www/secure.php
@@ -1,12 +1,29 @@
 <?php
-    session_start();
-    if(!isset($_SESSION["user_data"]["username"])){
-        header("location:http://147.210.12.1/index.php");
-        exit();
-    } 
+session_start();
+if(isset($_POST["submit"])){
+    $users = array(
+        "user" => "toto",
+        "admin"=>"password"
+    );
+    $username = isset($_POST["username"]) ? $_POST["username"] : "";
+    $password = isset($_POST["password"]) ? $_POST["password"] : "";
+    if(isset($users[$username]) && $password === $users[$username]){
+        $_SESSION["user_data"]["username"] = $username;
+        $msg = "Hello " . $username;
+    }else{
+        $msg = "Invalid Login";
+    }
+} else {
+    if(session_status() === PHP_SESSION_ACTIVE) {
+        $msg = "Welcome back, " . $_SESSION["user_data"]["username"];
+    } else {
+        $msg = "Not logged in";
+        header('Location: http://147.210.12.1/index.php');
+    }
+}
 ?>
 <html>
   <body>
-    <p> Hello, <?php echo $_SESSION["user_data"]["username"]; ?></p>
+    <p><?php echo $msg; ?></p>
   </body>
 </html>
