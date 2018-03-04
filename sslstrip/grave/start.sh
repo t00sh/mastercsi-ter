bf51ccbd907488f15ea1e343460dd6e12cbc7e3a
Simon Duret <simon.duret@etu.u-bordeaux.fr>
2018-03-04 16:49:49 +0100

    sslstrip+https-interception: add www.opeth.local to /etc/hosts + add CA certificate to firefox using certutil (start.sh)

--- sslstrip/grave/start.sh
+++ sslstrip/grave/start.sh
@@ -3,4 +3,7 @@
 ifconfig eth0 add 147.210.13.2 netmask 255.255.255.0 up
 route add default gw 147.210.13.1
 
-cp /mnt/host/cert.pem /etc/ssl/certs/
+echo "147.210.12.1 www.opeth.local" > /etc/hosts
+
+# Add CA certificate to firefox
+su - toto -c 'certutil -A -n "ca@ca.local" -t "C,C,C" -i /mnt/host/cert.pem -d /home/toto/.mozilla/firefox/*.default'
